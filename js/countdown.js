a69b1eb29b2cf199a5d7ec8d214bcc0039eebf02
JuanIgnacioTosetti <132720472+JuanIgnacioTosetti@users.noreply.github.com>
2023-10-12 19:32:06 -0300

    Actualizar countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuentaRegresiva', {
     year: 2023, // required
     month: 11, // required
     day: 11, // required
-    hours: 20, // Default is 0 [0-23] integer
+    hours: 21, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
